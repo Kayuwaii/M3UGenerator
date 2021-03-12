7483c2aad6135f11783ff3973e04398fe2d76042
ALEX CORONAS ALTEMIR <alexcoralt@msn.com>
2021-03-12 23:32:39 +0100

    Windows console app and crossplatform library

--- M3UGenerator/Program.cs
+++ M3UGenerator/Program.cs
@@ -1,14 +1,61 @@
 ﻿using M3UGeneratorCore;
 using System;
+using System.IO;
 
 namespace M3UGenerator
 {
-    internal class Program
+    public class M3UGenerator
     {
+        private static bool recursive = false;
+        private static bool help = false;
+        private static string ogPath = Directory.GetCurrentDirectory();
+        private static string destPath;
+        private static string outName;
+        private static EmulatedConsole selConsole;
+
         private static void Main(string[] args)
         {
-            M3UGeneratorCore.M3UGeneratorCore.SetConsole(EmulatedConsole.PSX);
-            M3UGeneratorCore.M3UGeneratorCore.GenerateM3U(@"G:\GAMES\ROMS\PSX\Persona 2 - Eternal Punishment", relative: true);
+            ParseParams(args);
+
+            M3UGeneratorCore.M3UGeneratorCore.SetConsole(selConsole);
+            M3UGeneratorCore.M3UGeneratorCore.GenerateM3U(ogPath, outName, destPath, recursive);
+        }
+
+        private static void ParseParams(string[] args)
+        {
+            foreach (var param in args)
+            {
+                if (param.StartsWith("-r")) recursive = true;
+                if (param.StartsWith("-h")) help = true;
+                else
+                {
+                    var parsedParam = param.Split("=");
+                    switch (parsedParam[0])
+                    {
+                        case "--ROMDirectory":
+                            ogPath = parsedParam[1];
+                            break;
+
+                        case "--FileName":
+                            outName = parsedParam[1];
+                            break;
+
+                        case "--OutputDirectory":
+                            destPath = parsedParam[1];
+                            break;
+
+                        case "--EmulationTarget":
+                            try
+                            {
+                                selConsole = Enum.Parse<EmulatedConsole>(parsedParam[1]);
+                            }
+                            catch (Exception ex)
+                            {
+                            }
+                            break;
+                    }
+                }
+            }
         }
     }
 }
